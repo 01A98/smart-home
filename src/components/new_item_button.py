4234120f3cdf56bc6ce8784e24d3cf52aca10036
Ola Koblass <Aleksander.Koblass@modernatx.com>
2024-05-26 23:02:50 +0200

    feat: add killswitch placeholder

--- src/components/new_item_button.py
+++ src/components/new_item_button.py
@@ -2,21 +2,21 @@ from dominate.tags import a, span
 
 
 class NewItemButton(a):
-    tagname = "a"
+    tagname = "span"
     id = "new-item-button"
 
     def __init__(self, href: str = None) -> None:
         super().__init__()
 
-        self["href"] = href
+        self["class"] = (
+            "text-white bg-pink-500 hover:bg-pink-600 focus:outline-none focus:ring-4 "
+            "focus:ring-purple-300 font-medium rounded-md text-md p-4 text-center"
+        )
+        self["role"] = "button"
+        self["aria-label"] = "Add new item"
 
         with self:
-            span(
+            a(
                 "+ Dodaj Nowy",
-                class_name="self-center text-white bg-pink-500 hover:bg-pink-600 focus:outline-none "
-                           "focus:ring-4 focus:ring-purple-300 font-medium rounded-md text-md px-4 py-3 "
-                           "text-center mb-4",
-                role="button",
-                aria_label="Add new item",
-
+                href=href,
             )
--- src/views/bulbs/bulbs.py
+++ src/views/bulbs/bulbs.py
@@ -1,22 +1,27 @@
 from itertools import groupby
 
-from sanic import Request, Sanic
+from sanic import Request, Sanic, html
 from sanic.views import HTTPMethodView
-from sanic_ext import render
+from sanic_ext import serializer
 from tortoise.transactions import atomic
+from dominate.tags import div, section, a, h2, p, small
 
 from src.components.spinner import Spinner
 from src.models.bulb import Bulb
 from src.views import NAVIGATION, Page, BaseContext
+from src.components.base_page import BasePage
+from src.components.breadcrumbs import Breadcrumbs
+from src.components.new_item_button import NewItemButton
+from src.components.nothing_here import NothingHere
+from src.components.bulb_icon import BulbIcon
 
 
 def create_view(app: Sanic) -> None:
     class BulbsView(HTTPMethodView):
-        decorators = [atomic()]
+        decorators = [atomic(), serializer(html)]
         page = Page(
             name="BulbsView",
             title="Żarówki",
-            template_path="views/bulbs/get.html",
         )
 
         async def get(self, request: Request):
@@ -31,14 +36,75 @@ def create_view(app: Sanic) -> None:
                 for room_name, bulbs in bulbs_by_room_name_grouped
             }
 
-            return await render(
-                self.page.template_path,
-                context={
-                    "bulbs_by_room_name": bulbs_by_room_name,
-                    "spinner": str(Spinner(htmx_indicator=True)),
-                    **BaseContext(app=app, current_page=self.page).model_dump(),
-                },
+            base_ctx = BaseContext(app=app, current_page=self.page)
+            navbar = base_ctx.app_navbar
+
+            page_content = BasePage(
+                navbar,
+                div(
+                    Breadcrumbs(app, base_ctx.navigation["home"], self.page),
+                    class_name="w-full max-w-screen-xl mx-auto p-2",
+                ),
+                section(
+                    div(
+                        NewItemButton(href=app.url_for("BulbView", id="new")),
+                        class_name="block mx-auto w-full max-w-screen-xl pt-6 py-6 px-2",
+                    ),
+                    div(
+                        *[
+                            self._room_section(room_name, bulbs, app)
+                            for room_name, bulbs in bulbs_by_room_name.items()
+                        ]
+                        or [NothingHere()],
+                        class_name="w-full h-full max-w-screen-xl py-6 px-4 mx-auto mx-4",
+                    ),
+                ),
+                title=self.page.title,
             )
+
+            return page_content.render()
+
+        @staticmethod
+        def _room_section(room_name, bulbs, app):
+            with div(
+                class_name="my-6 flex flex-col border border-gray-300 dark:border-gray-700 rounded-md p-8"
+            ):
+                a(
+                    h2(
+                        room_name,
+                        class_name="text-2xl font-bold text-gray-800 dark:text-gray-100",
+                    ),
+                    href=app.url_for("RoomView", id=bulbs[0].room_id)
+                    if bulbs[0].room
+                    else "#",
+                    class_name="self-end",
+                )
+                room_bulbs = [BulbsView._bulb_card(bulb, app) for bulb in bulbs]
+                return room_bulbs
+
+        @staticmethod
+        def _bulb_card(bulb, app):
+            with div(
+                class_name="my-8 min-w-fit rounded-md text-white bg-indigo-700 hover:bg-pink-700 flex flex-col shadow-gray-200 dark:shadow-gray-900 duration-300 hover:-translate-y-1"
+            ) as card:
+                with div(class_name="p-4 self-start"):
+                    a(
+                        bulb.name,
+                        class_name="text-lg hover:-translate-y-[2px] mb-4 font-semibold leading-relaxed text-gray-200 dark:text-white",
+                        href=app.url_for("BulbView", id=bulb.id),
+                    )
+                with div(id=f"bulb-info-{bulb.id}", class_name="px-2 mb-4"):
+                    div(
+                        **{
+                            "hx-get": app.url_for("bulb_with_state", id=bulb.id),
+                            "hx-trigger": "load",
+                            "hx-swap": "innerHTML",
+                            "hx-indicator": "app-spinner",
+                            "hx-target": f"#bulb-info-{bulb.id}",
+                        }
+                    )
+                    Spinner(htmx_indicator=True)
+            return card
 
     app.add_route(BulbsView.as_view(), "/bulbs")
     NAVIGATION["bulbs"] = BulbsView.page
