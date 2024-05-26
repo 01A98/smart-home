d7163b6ac259a3c3ba40130a19a34938871d9bb6
01A98 <aleksandra.koblass@gmail.com>
2024-05-26 23:52:45 +0200

    feat: update scripts to work in my rpi

--- dev.sh
+++ dev.sh
@@ -1,1 +1,1 @@
-source venv/bin/activate && ENV=development python -m sanic main:create_app --host 0.0.0.0 --port 8080 --debug --reload
+NV=development python3.11 -m sanic main:create_app --host 0.0.0.0 --port 8080 --debug --reload
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-source venv/bin/activate && ENV=production PYTHONUNBUFFERED=1 npx pm2 start main.py --interpreter=venv/bin/python --name smart-home-web-app
+ENV=production PYTHONUNBUFFERED=1 npx pm2 start main.py --name smart-home-web-app --interpreter=/usr/local/bin/python3.11
